efc85dd611b5003d7f5fcef998d24254e60015bc
gholib40 <ghoelibsoekamti@gmail.com>
2025-07-22 20:46:59 +0700

    whoob

--- src/useMovies.js
+++ src/useMovies.js
@@ -12,7 +12,7 @@ export function useMovies(query, callback) {
       const fetchMovies = async () => {
         try {
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           console.log(res);
